a9495b7f3b8a65e2dad179855cc70f555225ac48
busisani <53223225+busisani@users.noreply.github.com>
2020-05-01 22:19:56 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "New Orleans, USA",
+    29.95465, 
+    -90.07507
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Maryland, USA",
+    39.00039, 
+    -76.74997,
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Pretoria, South Africa",
+    -25.74486, 
+    28.18783
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Harare, Zimbabwe",
+    -17.82772, 
+    31.05337
   ]
 ];
